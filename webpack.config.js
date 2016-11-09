f4b9526234d0ba1c48d5eb29bb9065cfab20c0a4
Kate <yekaterina.filippova@gmail.com>
2016-11-08 22:38:09 -0500

    added configuration to babel/webpack

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+
+// Plugins
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+  	// this is where webpack will start looking. starting point of website
+    app: ['./app.js']
+  },
+
+  output: {
+    path: __dirname + '/dist',
+    publicPath: 'http://localhost:8080/'
+  },
+
+  devServer : {
+    port: 8080
+  },
+
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body'
+    })
+  ],
+
+  module: {
+
+    rules: [
+      /* preloaders */
+      // {
+      //   enforce: 'pre',
+      //   test: /\.js$/,
+      //   use: 'eslint-loader',
+      //   exclude: /node_modules/,
+      // },
+
+      /* loaders */
+      {
+        test: /\.js/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true
+        }
+      }
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader?sourceMap', 'css-loader', 'postcss-loader']
+      // }
+    ]
+  }
+};
