082c618b1fd2902e294d1a3c1f03731710bc755b
Kate <yekaterina.filippova@gmail.com>
2016-11-13 21:59:12 -0500

    first component

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 import newModule from "./exampleModule";
+
+// react-dom has to have react package loaded before it
+import React from "react";
+import reactDom from "react-dom";
+import Header from "./header.js";
+
 console.log("baby's second react code");
+
+// take the components and render them into the page
+// we use react-dom to render
+
+reactDom.render(<Header/>, document.body);
---
+++ header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const styling = {
+	height: 100,
+	width: '100%',
+	backgroundColor: 'black',
+	color: 'white',
+	padding: 10
+};
+
+// creating a new class that inherits from React which is the "react" component
+// think of the class as a super object
+class header extends React.Component {
+	// there is one particular function that has to be defined for a react component
+	// every react component has a render
+	render() {
+		// render is a function of the header class
+		// this function needs to return something, what is it rendering?
+		return (
+			// jsx goes here
+			<div style={styling}>
+				HEADER
+			</div>
+		)
+	}	
+}
+
+// when we say "class header", that creates a variable that we can export
+export default header;
